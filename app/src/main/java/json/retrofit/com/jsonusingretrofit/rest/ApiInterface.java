3b159d87018b8694e9716c49a6b293624641e150
Abhi <abhijeet.tank@gmail.com>
2017-08-08 17:44:41 +0530

    Initial commit

---
+++ app/src/main/java/json/retrofit/com/jsonusingretrofit/rest/ApiInterface.java
@@ -0,0 +1,21 @@
+package json.retrofit.com.jsonusingretrofit.rest;
+
+/**
+ * Created by Nitin on 14/07/17.
+ */
+
+
+import json.retrofit.com.jsonusingretrofit.model.MoviesResponse;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+
+public interface ApiInterface {
+    @GET("movie/top_rated")
+    Call<MoviesResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+
+    @GET("movie/{id}")
+    Call<MoviesResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+}
